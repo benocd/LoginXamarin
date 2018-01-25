b5c74539fb7d436c54bbf9065fdd4345b2de5864
Bernardo Campos <bcampos@iqs.cl>
2018-01-25 11:16:40 -0300

    Add project files.

---
+++ MeetingsMobile/MeetingsMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MeetingsMobile.Models;
+
+namespace MeetingsMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
